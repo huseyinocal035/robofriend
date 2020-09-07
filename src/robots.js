72ab8195564dd0b20302ed0676df0bf1bd77dabd
huseyinocal035 <huseyinocal035@gmail.com>
2020-09-07 18:07:37 +0300

    second commit

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Hüseyin Öcal',
+        username: 'hsynocal',
+        email: 'huseyinocal035@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Yelda Güreşmen Öcal',
+        username: 'yelda',
+        email: 'yeldaguresmen@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Okan Öcal',
+        username: 'okan',
+        email: 'okanocal035@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Mustafa Öcal',
+        username: 'mustafa',
+        email: 'mustafa@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Recep Tayyip Erdoğan',
+        username: 'rte',
+        email: 'rte@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Abdurrezzak Koca',
+        username: 'abdurrezzak',
+        email: 'abdurrezzak@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Ali Öcal',
+        username: 'aliocal',
+        email: 'aliocal@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Figen Öcal',
+        username: 'fgnocal',
+        email: 'fgn@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Mustafa Kocabaş',
+        username: 'mustafakcbs',
+        email: 'mustafakcbs@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Reyhan Kocabaş',
+        username: 'ryhnkcbs',
+        email: 'ryhnkcbs@gmail.com'
+    }
+
+]
